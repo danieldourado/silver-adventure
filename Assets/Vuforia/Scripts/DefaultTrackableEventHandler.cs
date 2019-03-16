da3ee1ffe7ec94ca99bbeb7ce4a26c7419d53435
Daniel Dourado <danieldourado@Arthurs-MacBook-Pro.local>
2019-03-16 12:57:49 -0300

    adjusted the sphere scenes to go back to the camera scene

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -44,6 +44,8 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
 
     #region PUBLIC_METHODS
 
+    public GameObject instructions;
+
     /// <summary>
     ///     Implementation of the ITrackableEventHandler function called when the
     ///     tracking state changes.
@@ -61,12 +63,14 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         {
             Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
             OnTrackingFound();
+            instructions.SetActive(false);
         }
         else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
                  newStatus == TrackableBehaviour.Status.NO_POSE)
         {
             Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
             OnTrackingLost();
+            instructions.SetActive(true);
         }
         else
         {
