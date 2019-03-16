2025052d3dd094d7a120433fc53d2ca2ab1ca49c
Daniel D. Fernandes <daniel.dourado@gmail.com>
2019-03-16 18:50:58 -0300

    moved scripts to correct folder

---
+++ Assets/src/SphereHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereHelper : MonoBehaviour
+{
+    public Material skyBox;
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
